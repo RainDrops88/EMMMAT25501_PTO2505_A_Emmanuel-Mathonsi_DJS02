0d25390f4512f3c5756400f748ad8d49c1b9ea7c
Emmanuel <07942jerry@gmail.com>
2026-02-13 19:38:22 +0200

    creating function to render the card

---
+++ scripts/index.js
@@ -0,0 +1,22 @@
+import { PodcastCard } from "./components/createCard.js";
+import { podcasts } from "./data.js";
+
+const podcastCardsContainer = document.getElementById('podcast-container');
+
+/**
+ * Renders all podcast cards into the podcast cards container and modal.
+ * 
+ */
+function renderPodcastCards() {
+    podcastCardsContainer.innerHTML = ''; // Clear existing cards
+    podcasts.forEach(podcast => {
+        const card = new PodcastCard();
+        card.setPodcast(podcast);
+        podcastCardsContainer.appendChild(card);
+    });
+}
+
+
+// Initial rendering of podcast cards
+
+renderPodcastCards();
